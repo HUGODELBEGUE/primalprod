c3804a0ce4f8998c6cfe94794f8187cd3ce1ba08
Hugo Delbegue <hugodelbegue@gmail.com>
2022-07-26 13:36:35 +0200

    remise en forme des documents

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var bouton = document.querySelector("#icon-menu");
 var icon = document.querySelector("#icon-menu > i");
 bouton.addEventListener("click", () => {
   if (icon.className === 'bi bi-list') {
-icon.classList.remove('bi-list');
-icon.classList.add('bi-x');
+    icon.classList.remove('bi-list');
+    icon.classList.add('bi-x');
   } else {
-icon.classList.remove('bi-x');
-icon.classList.add('bi-list');
+    icon.classList.remove('bi-x');
+    icon.classList.add('bi-list');
   }
 });
