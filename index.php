ac300a2dbbc9f47989783597886ddbc291f76132
Hugo Delbegue <hugodelbegue@gmail.com>
2022-09-16 22:20:26 +0200

    modification projet teacozy

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!--    <meta name="description" content="Site web PrimalProd">-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
     <link rel="stylesheet" href="assets/css/variables.css" type="text/css">
