0274572829896bd28109377d5eafa9fc758ff3b4
Hugo Delbegue <hugodelbegue@gmail.com>
2022-10-01 18:20:55 +0200

    legalite

--- templates/confidentialite.html.php
+++ templates/confidentialite.html.php
@@ -3,44 +3,34 @@
 <div class="official_doc">
 
     <h2>Politique de confidentialité</h2>
-    <p>Le site web https://www.primalprod.fr/ appartient à la micro-entreprise PrimalProd, qui est représenté par
-        Hugo
-        Delbegue. Son siège social est situé au 5 rue Lachmann, 38000 Grenoble. PrimalProd est amenée à collecter et
-        à
-        traiter des informations dont certaines sont qualifiées de “données
-        personnelles”. Hugo Delbegue attache une grande importance au respect de la vie privée, et n’utilise que des
+    <p>Le site web <a href="https://primalprod.fr/">www.primalprod.fr</a> appartient à la micro-entreprise PrimalProd, qui est représenté par
+        Hugo Delbegue. Son siège social est situé au 5 rue Lachmann, 38000 Grenoble. PrimalProd est amenée à collecter et
+        à traiter des informations dont certaines sont qualifiées de “données personnelles”. Hugo Delbegue attache une grande importance au respect de la vie privée, et n’utilise que des
         données de manière responsable et confidentielle et dans une finalité précise.</p>
 
     <h2>Les informations personnelles</h2>
-    <p>Lorsque vous visitez le site www.primalprod.fr, un seul type de données transmises directement est
-        susceptibles
-        d’être recueillies.
-        <br>
+    <p>Lorsque vous visitez le site <a href="https://primalprod.fr/">www.primalprod.fr</a>, un seul type de données transmises directement est
+        susceptibles d’être recueillies.
+        <br><br>
         Ces données sont celles que vous nous transmettez directement, via un formulaire de contact ou par contact
-        direct
-        par email. Elles sont obligatoires dans les champs « VOTRE NOM » et « VOTRE ADRESSE EMAIL » du formulaire.
+        direct par email. Elles sont obligatoires dans les champs « VOTRE NOM » et « VOTRE ADRESSE EMAIL » du formulaire.
     </p>
 
     <h2>L'utilisation de vos données</h2>
     <p>Notre priorité absolue est la sécurité des données des clients et, à ce titre, nous ne pouvons traiter que
-        des
-        données minimales sur les utilisateurs, uniquement dans la mesure où cela est absolument nécessaire pour le
-        site
-        web.
-        <br>
+        des données minimales sur les utilisateurs, uniquement dans la mesure où cela est absolument nécessaire pour le
+        site web.
+        <br><br>
         Les données que vous nous transmettez directement sont utilisées dans le but de vous re-contacter et/ou dans
-        le
-        cadre de la demande que vous nous faites. Ces données sont utilisées par Hugo Delbegue, responsable du
-        traitement
-        des données, et ne seront jamais cédées à un tiers ni utilisées à d’autres fins que celles détaillées
+        le cadre de la demande que vous nous faites. Ces données sont utilisées par Hugo Delbegue, responsable du
+        traitement des données, et ne seront jamais cédées à un tiers ni utilisées à d’autres fins que celles détaillées
         ci-dessus.
     </p>
 
     <h2>Base légale</h2>
     <p>Les données personnelles ne sont collectées qu’après consentement obligatoire de l’utilisateur. Ce
         consentement
-        est
-        valablement recueilli (description et bouton à cliquer), libre, clair et sans équivoque.</p>
+        est valablement recueilli (description et bouton à cliquer), libre, clair et sans équivoque.</p>
 
     <h2>Durée de conservation</h2>
     <p>Vos données seront conservées durant une durée maximale de 3 ans.</p>
@@ -50,39 +40,25 @@
 
     <h2>Liens vers d'autres sites web</h2>
     <p>Notre site web peut contenir des liens vers d'autres sites web qui ne sont pas détenus ou contrôlés par nous.
-        Sachez
-        que
-        nous ne sommes pas responsables de ces autres sites web ou des pratiques de confidentialité des tiers. Nous
-        vous
-        encourageons à être attentif lorsque vous quittez notre site web et à lire les déclarations de
-        confidentialité
-        de
-        chaque
-        site web susceptible de collecter des informations personnelles.</p>
+        Sachez que nous ne sommes pas responsables de ces autres sites web ou des pratiques de confidentialité des tiers. Nous
+        vous encourageons à être attentif lorsque vous quittez notre site web et à lire les déclarations de
+        confidentialité de chaque site web susceptible de collecter des informations personnelles.</p>
 
     <h2>Divulgation légale</h2>
     <p>Nous divulguerons toute information que nous collectons, utilisons ou recevons si la loi l'exige ou
-        l'autorise,
-        par
-        exemple pour nous conformer à une citation à comparaître ou à une procédure judiciaire similaire, et lorsque
-        nous
-        pensons de bonne foi que la divulgation est nécessaire pour protéger nos droits, votre sécurité ou celle
-        d'autrui,
-        enquêter sur une fraude ou répondre à une demande du gouvernement.</p>
+        l'autorise, par exemple pour nous conformer à une citation à comparaître ou à une procédure judiciaire similaire, et lorsque
+        nous pensons de bonne foi que la divulgation est nécessaire pour protéger nos droits, votre sécurité ou celle
+        d'autrui, enquêter sur une fraude ou répondre à une demande du gouvernement.</p>
 
-    <h2>Changement</h2>
+    <h2>Changements</h2>
     <p>Nous pouvons être amenés à modifier la présente politique de confidentialité de temps à autre afin d'y
-        refléter,
-        par
-        exemple, les changements apportés à nos pratiques ou pour d'autres motifs opérationnels, juridiques ou
+        refléter, par exemple, les changements apportés à nos pratiques ou pour d'autres motifs opérationnels, juridiques ou
         réglementaires.</p>
 
     <h2>Contact</h2>
     <p>Si vous souhaitez nous contacter pour comprendre davantage la présente politique ou si vous souhaitez nous
-        contacter
-        concernant toute question relative aux droits individuels et à vos informations personnelles, vous pouvez
-        envoyer un
-        e-mail à l'adresse hugodelbegue@gmail.com.</p>
+        contacter concernant toute question relative aux droits individuels et à vos informations personnelles, vous pouvez
+        envoyer un e-mail à l'adresse hugodelbegue@gmail.com.</p>
 
 </div>
 
